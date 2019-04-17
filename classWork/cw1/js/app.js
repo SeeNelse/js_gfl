09e53020c020b7c8cc5e3e7c9fc02762005edc03
student <student@GFL-1505.gfl.geeksforless.net>
2019-04-17 18:59:13 +0300

    add cw1-2

---
+++ classWork/cw1/js/app.js
@@ -0,0 +1,48 @@
+// var obj1 = {
+//   id: 0,
+//   name: 'name1'
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//   obj1.id = i;
+//   console.log(obj1);
+// }
+
+// for(var i in obj1) {
+//   console.log(i);
+// }
+
+// arr1 = [1,2,3,'f', '4324'];
+
+// arr1.forEach(function(val, index){
+//   console.log(val, index);
+// });
+
+// function calcA(a) {
+//   return function (b) {
+//     return a + b;
+//   }
+// }
+
+
+// function testClass() {
+//   this.run = function() {
+//     console.log('123');
+//   }
+// }
+
+// testClass.prototype.abc = '123';
+
+// testClass.prototype.run = function () {
+  //   console.log('method run');
+  // }
+  
+  // var app = new testClass();
+  
+  // app.run();
+  
+  
+function calcFunc(a, b) {
+  return a * b;
+}
+
