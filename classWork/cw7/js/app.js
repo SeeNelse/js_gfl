fb81a3dbf5aeee3fb7cd1e7765dd50f609bdcff9
student <student@GFL-1505.gfl.geeksforless.net>
2019-04-23 21:51:22 +0300

    add cw7

---
+++ classWork/cw7/js/app.js
@@ -0,0 +1,76 @@
+(function(){
+  ingredients = [
+    {
+      id: 1,
+      name: 'milk',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'bread',
+      price: 200,
+    },
+    {
+      id: 3,
+      name: 'oil',
+      price: 300,
+    },
+    {
+      id: 4,
+      name: 'apple',
+      price: 400,
+    },
+    {
+      id: 5,
+      name: 'carrot',
+      price: 500,
+    },
+    {
+      id: 6,
+      name: 'banana',
+      price: 600,
+    },
+    {
+      id: 65,
+      name: 'teeeeeest',
+      price: 600,
+    },
+  ];
+
+  new Vue({
+    el: '#app',
+    data: function() {
+      return {
+        ingrArr: ingredients,
+        cart: JSON.parse(localStorage.cart || '[]'),
+        cartArr: [],
+      }
+    },
+    methods: {
+      add: function (key, item) {
+        
+
+
+        // this.cart['id-'+item.id] ? this.cart['id-'+item.id]++ : this.cart['id-'+item.id] = 1;
+        // for (key in this.cart) {
+        //   this.ingrArr.forEach(el => {
+        //     if (el.id === +key.substring(3)) {
+        //       el['count'] = this.cart['id-'+item.id];
+        //       if (el['count'] === 1) {
+        //         this.cartArr.push(el);
+        //       }
+        //       console.log(el['count']);
+        //     }
+        //   });
+        // }
+      },
+    },
+    watch: {
+      cart: function(newVal) {
+        localStorage.cart = JSON.stringify(newVal);
+      }
+    }
+  });
+
+
+}())
