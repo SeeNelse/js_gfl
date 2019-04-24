170f379597825a421a64062362c6987dc95edb04
Vladislav <seenelse@gmail.com>
2019-04-24 13:30:19 +0300

    cw7 done

--- classWork/cw7/js/app.js
+++ classWork/cw7/js/app.js
@@ -5,78 +5,68 @@
       id: 1,
       name: 'milk',
       price: 100,
+      count: 0,
     },
     {
       id: 2,
       name: 'bread',
       price: 200,
+      count: 0,
     },
     {
       id: 3,
       name: 'oil',
       price: 300,
+      count: 0,
     },
     {
       id: 4,
       name: 'apple',
       price: 400,
+      count: 0,
     },
     {
       id: 5,
       name: 'carrot',
       price: 500,
+      count: 0,
     },
     {
       id: 6,
       name: 'banana',
       price: 600,
+      count: 0,
     },
     {
       id: 65,
       name: 'teeeeeest',
       price: 600,
+      count: 0,
     },
   ];
 
   new Vue({
     el: '#app',
-    data: function() {
-      return {
-        ingrArr: ingredients,
-        cart: JSON.parse(localStorage.cart || '[]'),
-        cartArr: [],
-      }
+    data: {
+      ingrArr: JSON.parse(localStorage.ingr || JSON.stringify(ingredients)),
     },
     methods: {
       add: function (key, item) { 
-        let _this = this;
-        if (!Object.keys(this.cart).length) {
-          this.ingrArr.forEach(function(el) {
-            this.cartArr[el.id]
-          });
-          console.log(this.cartArr);
-        }
-        console.log(this.cartArr);
-
-        // this.cart['id-'+item.id] ? this.cart['id-'+item.id]++ : this.cart['id-'+item.id] = 1;
-        // for (key in this.cart) {
-        //   this.ingrArr.forEach(el => {
-        //     if (el.id === +key.substring(3)) {
-        //       el['count'] = this.cart['id-'+item.id];
-        //       if (el['count'] === 1) {
-        //         this.cartArr.push(el);
-        //       }
-        //       console.log(el['count']);
-        //     }
-        //   });
-        // }
+        item.count++;
+        localStorage.ingr = JSON.stringify(this.ingrArr);
       },
     },
-    watch: {
-      cart: function(newVal) {
-        localStorage.cart = JSON.stringify(newVal);
-      }
-    }
+    computed: {
+      total__price: function() {
+        let total = 0;
+        this.ingrArr.forEach((el) => {
+          if (el.count) {
+            total += el.price * el.count;
+          }
+        });
+        return total;
+      },
+    },
   });
 
 }())
